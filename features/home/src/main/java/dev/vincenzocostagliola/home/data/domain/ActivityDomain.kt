315a89fea1ff4d5f7f50daae4f222d5f1845b9cc
vince <vincenzo.costagliola@develhope.co>
2025-05-06 22:28:22 +0200

    fixed ActivityDomain

--- features/home/src/main/java/dev/vincenzocostagliola/home/data/domain/ActivityDomain.kt
+++ features/home/src/main/java/dev/vincenzocostagliola/home/data/domain/ActivityDomain.kt
@@ -1,3 +1,8 @@
 package dev.vincenzocostagliola.home.data.domain
 
-data class ActivityDomain()
+data class ActivityDomain(
+    val id : Int,
+    val title : String,
+    val description : String,
+    val status : String
+)
