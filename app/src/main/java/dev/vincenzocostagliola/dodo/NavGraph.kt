bc64d29e597f515575f6213ef7b37f36b76a1a72
vince <vincenzo.costagliola@develhope.co>
2025-05-06 22:25:58 +0200

    added Home

--- app/src/main/java/dev/vincenzocostagliola/dodo/NavGraph.kt
+++ app/src/main/java/dev/vincenzocostagliola/dodo/NavGraph.kt
@@ -3,20 +3,11 @@ package dev.vincenzocostagliola.dodo
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
-import dev.vincenzocostagliola.coindetails.ui.DescriptionScreen
+import dev.vincenzocostagliola.data.navigation.NavigationRoute
 import dev.vincenzocostagliola.home.ui.HomeScreen
 import dev.vincenzocostagliola.home.ui.HomeViewModel
-import dev.vincenzocostagliola.data.navigation.NavigationRoute
-import dev.vincenzocostagliola.coindetails.ui.DetailsScreen
-import dev.vincenzocostagliola.coindetails.ui.DetailsScreenViewModel
-import timber.log.Timber
-import java.net.URLDecoder
-import java.nio.charset.StandardCharsets
-import kotlin.let
 
 
 @Composable
@@ -27,7 +18,7 @@ fun NavGraph(navController: NavHostController) {
             val navigateToDetail : () -> Unit = {  }
             HomeScreen(viewModel, navigateToDetail)
         }
-
+/*
         composable(
             route = NavigationRoute.DetailsScreen.route,
             arguments = listOf(navArgument(NavigationRoute.DetailsScreen.argumentId) {
@@ -63,6 +54,8 @@ fun NavGraph(navController: NavHostController) {
             val onBackPressed: () -> Unit = { navController.popBackStack() }
             DescriptionScreen(description, onBackPressed)
         }
+
+ */
 
     }
 }
