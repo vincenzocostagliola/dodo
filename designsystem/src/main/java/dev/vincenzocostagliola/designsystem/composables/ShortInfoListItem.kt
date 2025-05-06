4da21401a3dff3d38f25d02afaae992065f3abdf
vince <vincenzo.costagliola@develhope.co>
2025-05-06 17:23:42 +0200

    fixed ShortInfoListItem

--- designsystem/src/main/java/dev/vincenzocostagliola/designsystem/composables/ShortInfoListItem.kt
+++ designsystem/src/main/java/dev/vincenzocostagliola/designsystem/composables/ShortInfoListItem.kt
@@ -8,38 +8,56 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.AsyncImage
 
 data class InfoUi(
-    val currentPrice: Double,
-    val id: String,
-    val image: String,
-    val marketCap: Double,
-    val marketCapRank: Int,
+    val id: Int,
+    val description: String,
     val name: String,
+    val status: String,
+    val image: String?
 )
 
 @Composable
-fun ShortInfoListItem(coin: InfoUi, onClick: (String) -> Unit) {
+fun ShortInfoListItem(info: InfoUi, onClick: (Int) -> Unit) {
     Surface(
-        onClick = { onClick(coin.id) }
+        onClick = { onClick(info.id) }
     ) {
         Column {
             ListItem(
-                headlineContent = { Text(coin.name) },
-                supportingContent = { Text(coin.currentPrice.toString()) },
-                trailingContent = { Text(coin.marketCapRank.toString()) },
+                headlineContent = { Text(info.name) },
+                supportingContent = { Text(info.description) },
+                trailingContent = { Text(info.status) },
                 leadingContent = {
-                    AsyncImage(
-                        model = coin.image,
-                        modifier = Modifier,
-                        contentScale = ContentScale.Fit,
-                        contentDescription = coin.name
-                    )
+                    info.image?.let {
+                        AsyncImage(
+                            model = info.image,
+                            modifier = Modifier,
+                            contentScale = ContentScale.Fit,
+                            contentDescription = info.name
+                        )
+                    }
                 }
             )
             HorizontalDivider()
-
         }
     }
 }
+
+
+@Preview
+@Composable
+private fun ShowShortInfoListItem() {
+    val info: InfoUi = getFakeInfo()
+
+    ShortInfoListItem(info) {}
+}
+
+private fun getFakeInfo(): InfoUi = InfoUi(
+    id = 1,
+    description = "This is Description",
+    name = "What to do at home",
+    status = "Doing",
+    image = null
+)
