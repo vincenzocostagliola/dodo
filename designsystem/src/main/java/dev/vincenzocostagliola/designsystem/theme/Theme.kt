58adeb7dd5c714515f84949689d74914be051f2c
vince <vincenzo.costagliola@develhope.co>
2025-05-06 17:42:09 +0200

    fixed theme

--- designsystem/src/main/java/dev/vincenzocostagliola/designsystem/theme/Theme.kt
+++ designsystem/src/main/java/dev/vincenzocostagliola/designsystem/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun CoinswatchTheme(
+fun DodoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
