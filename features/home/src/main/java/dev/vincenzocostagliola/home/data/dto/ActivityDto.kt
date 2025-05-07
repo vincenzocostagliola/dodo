b9e94da97055d6a62853cfab79f18c29b4a7e22f
vince <vincenzo.costagliola@develhope.co>
2025-05-07 16:29:03 +0200

    added home usecase

--- features/home/src/main/java/dev/vincenzocostagliola/home/data/dto/ActivityDto.kt
+++ features/home/src/main/java/dev/vincenzocostagliola/home/data/dto/ActivityDto.kt
@@ -1,8 +1,19 @@
 package dev.vincenzocostagliola.home.data.dto
 
+import dev.vincenzocostagliola.home.data.domain.ActivityDomain
+
 internal data class ActivityDto(
     val id : Int,
     val title : String,
     val description : String,
     val status : String
-)
+){
+    fun toDomain() : ActivityDomain {
+        return ActivityDomain(
+            id = id,
+            title = title,
+            description = description,
+            status = status
+        )
+    }
+}
--- features/home/src/main/java/dev/vincenzocostagliola/home/usecase/HomeUseCase.kt
+++ features/home/src/main/java/dev/vincenzocostagliola/home/usecase/HomeUseCase.kt
@@ -1,13 +1,26 @@
 package dev.vincenzocostagliola.home.usecase
 
+import dev.vincenzocostagliola.home.data.domain.result.GetActivityResult
 import dev.vincenzocostagliola.home.data.repository.Repository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 internal interface HomeUseCase {
+    suspend fun getAllActivities(): Flow<GetActivityResult>
 }
 
 internal class HomeUseCaseImpl @Inject internal constructor(
     private val repository: Repository
 ) : HomeUseCase {
 
+    override suspend fun getAllActivities(): Flow<GetActivityResult> {
+        return flow {
+            repository.getAllActivities().collect {
+                GetActivityResult.Success(it.map { it.toDomain() })
+            }
+        }
+    }
 }
+
+
