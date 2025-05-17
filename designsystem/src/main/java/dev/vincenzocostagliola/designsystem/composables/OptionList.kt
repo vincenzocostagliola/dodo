b8f7aa304ce3e42346265cb4fd43a023fc7cbf6b
vince <vincenzo.costagliola@develhope.co>
2025-05-17 17:51:36 +0200

    refactored OptionList.kt

--- designsystem/src/main/java/dev/vincenzocostagliola/designsystem/composables/OptionList.kt
+++ designsystem/src/main/java/dev/vincenzocostagliola/designsystem/composables/OptionList.kt
@@ -3,7 +3,6 @@ package dev.vincenzocostagliola.designsystem.composables
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
